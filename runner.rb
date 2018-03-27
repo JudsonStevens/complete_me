0b87691af1c496fb22926fb064ef203e33867f39
JamisonOrdway <jamisonordway@gmail.com>
2018-03-27 16:09:58 -0600

    Adds runner file WIP

--- runner.rb
+++ runner.rb
@@ -6,6 +6,11 @@ class Shoes
 Shoes.app do
   stack do
     para "Type something here."
+
+  # I need a box here that takes user input
+  # @cm should search each word, every time the user presses a key
+  # @cm takes this in using populate method
+
   end
 end
 
