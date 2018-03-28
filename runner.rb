147b4a4695a70ec87ab05dd45159db96b83424f6
Judson Stevens <Judson.Stevens2gmail.com>
2018-03-27 21:52:36 -0600

    Nothing changed

--- runner.rb
+++ runner.rb
@@ -1,32 +1,28 @@
 require "./lib/complete_me"
 require "./lib/node"
+require 'green_shoes'
 
 @cm = CompleteMe.new
-class Shoes
 
-Shoes.app do
-  stack do
-    para "Type something here."
-    flow do
-      @user_input = edit_box
-      @suggestions = []
-      @user_input.change do
-        @suggestions.replace(@cm.suggest(@user_input.text).join(", "))
+  Shoes.app do
+    stack do
+      para "Type something here."
+      flow do
+        @user_input = edit_box
+        @suggestions = []
+        @user_input.change do
+          @suggestions.replace(@cm.suggest(@user_input.text).join(", "))
+        end
       end
 
+      flow do
+        @suggestions = para ''
+      end
+
+    # I need a box here that takes user input
+
+    # @cm should search each word, every time the user presses a key
+    # @cm takes this in using populate method
+
     end
-
-    flow do
-  @suggestions = para ''
-end
-
-  # I need a box here that takes user input
-
-  # @cm should search each word, every time the user presses a key
-  # @cm takes this in using populate method
-
   end
-end
-
-
-end
